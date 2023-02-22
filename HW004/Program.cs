c638480072fa980b84350781cd2eac82ad7c4724
Evgeniya <kapatoy@yandex.ru>
2023-02-22 14:09:52 +0300

    HW004

---
+++ HW004/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 2;
+Console.WriteLine("Все четные числа от 1 до " + n + ":");
+while (a <= n)
+{
+
+    Console.WriteLine(a);
+    a = a + 2;
+}
